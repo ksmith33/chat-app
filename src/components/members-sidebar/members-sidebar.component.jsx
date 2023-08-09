1568d289d84d3716570b42b554afad6e22818982
Keshyra <ksmith33@mail.umw.edu>
2023-08-08 21:51:19 -0400

    changed styling for members sidebar

--- src/components/members-sidebar/members-sidebar.component.jsx
+++ src/components/members-sidebar/members-sidebar.component.jsx
@@ -2,9 +2,9 @@ import './members-sidebar.styles.scss'
 function MembersSidebar({members}) {
 	return (
 		<div className="members-sidebar-container">
-			<h2>Members</h2>
+			 <h2>Members</h2>
 			{
-				members && members.map(member => {
+				members.map(member => {
 					return (
 						<div className='member-details'>
 							<img src={member.photo} alt={member.displayName}/>
